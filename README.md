1dbfa9a15e62e9595155da6c7dff16d5d745eae3
Gustavo Saiani <gustavo@poe.ma>
2017-01-29 15:15:29 -0200

    Add setup files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Retyping Pointing Poker in Elm and Firebase
+
+Just retyping this project to study Elm. For the real thing, [go here](https://github.com/liubko/elm-pointing-poker).
---
+++ config.js
@@ -0,0 +1,6 @@
+// for quick setup ¯\_(ツ)_/¯
+module.exports = {
+    apiKey: "AIzaSyCvuZ4IG2jWnhOvReQrS1DsG97qhohP0yk",
+    authDomain: "poker-94672.firebaseio.com",
+    databaseURL: "https://poker-94672.firebaseio.com"
+};
